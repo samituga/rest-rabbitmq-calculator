fe2f9228d6305ab97e5f6d2a6f4e930999fe00b9
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2021-04-26 21:34:15 +0100

    Configurations

---
+++ Calculator/src/main/java/com/perso/calculator/configurations/RabbitMQConfig.java
@@ -0,0 +1,51 @@
+package com.perso.calculator.configurations;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.queue}")
+    public static String queue;
+
+    @Value("${spring.rabbitmq.exchange}")
+    public static String exchange;
+
+    @Value("${spring.rabbitmq.routing-key}")
+    public static String routingKey;
+
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+}
